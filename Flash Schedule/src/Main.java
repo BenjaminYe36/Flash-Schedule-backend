46a04815680798792bcac1bca609af62b796d1c9
ygy36 <ygy36@HPS>
2020-11-26 20:56:37 -0800

    For testing purposes only

---
+++ Flash Schedule/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Testing for MeetingTimes class
+//		System.out.println(new MeetingTimes("TTh", "630-720"));
+//		System.out.println(new MeetingTimes("MWF", "730-820"));
+//		System.out.println(new MeetingTimes("MWF", "830-920"));
+//		System.out.println(new MeetingTimes("MWF", "930-1020"));
+//		System.out.println(new MeetingTimes("MWF", "1030-1120"));
+//		System.out.println(new MeetingTimes("MWF", "1130-1220"));
+//		System.out.println(new MeetingTimes("MWF", "1230-120"));
+//		System.out.println(new MeetingTimes("MWF", "130-220"));
+//		System.out.println(new MeetingTimes("MWF", "230-320"));
+//		System.out.println(new MeetingTimes("MWF", "330-420"));
+//		System.out.println(new MeetingTimes("MWF", "430-520P"));
+//		System.out.println(new MeetingTimes("MWF", "530-620P"));
+//		System.out.println(new MeetingTimes("MWF", "630-720P"));
+//		System.out.println(new MeetingTimes("MWF", "730-820P"));
+//		System.out.println(new MeetingTimes("MWF", "830-920P"));
+//		System.out.println(new MeetingTimes("MWF", "930-1020P"));
+//		
+//		System.out.println(new MeetingTimes("MWF", "1130-120"));
+//		System.out.println(new MeetingTimes("MWF", "1130-220"));
+//		System.out.println(new MeetingTimes("MWF", "1130-320"));
+//		System.out.println(new MeetingTimes("MWF", "1130-420"));
+	}
+}
