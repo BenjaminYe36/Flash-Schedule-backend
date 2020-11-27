0919c5f8ad02a2b27abaac4c170efa3efeb8201c
ygy36 <ygy36@HPS>
2020-11-27 00:29:24 -0800

    Testing loading from CSV success

--- Flash Schedule/src/Main.java
+++ Flash Schedule/src/Main.java
@@ -1,7 +1,9 @@
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// Testing for MeetingTimes class
 //		System.out.println(new MeetingTimes("TTh", "630-720"));
 //		System.out.println(new MeetingTimes("MWF", "730-820"));
@@ -24,5 +26,40 @@ public class Main {
 //		System.out.println(new MeetingTimes("MWF", "1130-220"));
 //		System.out.println(new MeetingTimes("MWF", "1130-320"));
 //		System.out.println(new MeetingTimes("MWF", "1130-420"));
+
+		// Testing for reading from CSV
+		List<Course> courseList = new ArrayList<>();
+		Course currCourse = null;
+		Lecture currLecture = null;
+		try (Scanner input = new Scanner(new File("test.csv"))) {
+			while (input.hasNextLine()) {
+				String[] res = input.nextLine().split(",");
+				switch (res[0]) {
+				case "C":
+					currCourse = Course.fromCSV(res);
+					courseList.add(currCourse);
+					break;
+				case "L":
+					currLecture = Lecture.fromCSV(res);
+					currCourse.addLecture(currLecture);
+					break;
+				case "Q":
+					Quiz tempQuiz = Quiz.fromCSV(res);
+					currLecture.addQuiz(tempQuiz);
+					break;
+				default:
+					System.out.println("No match and is an ERROR");
+				}
+			}
+		}
+		for (Course c : courseList) {
+			System.out.println(c);
+			for (Lecture l : c.getLectures()) {
+				System.out.println(l);
+				for (Quiz q : l.getQuizs()) {
+					System.out.println(q);
+				}
+			}
+		}
 	}
 }
