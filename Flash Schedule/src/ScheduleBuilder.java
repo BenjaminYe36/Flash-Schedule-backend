1d77461a9e00774fa98f8e2f7196b3b8db4e2bd0
nawZeroX239 <naw122333444455555@gmail.com>
2020-11-30 01:54:29 -0800

    done but need further testing

--- Flash Schedule/src/ScheduleBuilder.java
+++ Flash Schedule/src/ScheduleBuilder.java
@@ -10,6 +10,8 @@ import java.util.*;
  *
  */
 public class ScheduleBuilder {
+	
+	
 	/**
 	 * @param sc          See SelectedCourses class
 	 * @param constraints the object that stores all the filters
@@ -18,29 +20,90 @@ public class ScheduleBuilder {
 	 *         <p>
 	 *         When there is no possible schedules, it will return an empty Set
 	 */
-	public static Set<Set<Combo>> generateSelected(SelectedCourses sc, Constraints constraints) {
-		// TODO: finish implementation
-		// This can be done with loops or recursion.
-		// With recursion, may be some set places needs to be changed to list
-		// ! ! ! Remember to convert everything into Combo for convenience ! ! !
-		// Watch out for reference semantics, and make copies when necessary
+	public static Set<Set<Combo>> generateSchedules(SelectedCourses sc, Constraints constraints) {
+		
+		// use map to separate 
+		Map<String, Set<Combo>> courses = new HashMap<>(); // courses that must be in schedule
+		
+		if(sc.getCourses() != null) {
+			for(Course course : sc.getCourses()) {
+				courses.put(course.getCourseID(), course.validCombinations(constraints));
+			}
+		}
+		
+		if(sc.getLectures() != null) {
+			for(Map.Entry<Lecture, String> entry : sc.getLectures().entrySet()) {
+				// extracts course code, prefix and title
+				String coursePrefix="", courseCode="", value = entry.getValue();
+				String title = value.substring(value.indexOf(':') + 1);
+				Lecture lecture = entry.getKey();
+				for(int i=0; i<value.length(); ++i) {
+					char ch = value.charAt(i);
+					if(ch == ':')  break;
+					if(ch == ' ') continue;
+					if(ch >= '0' && ch <= '9') {
+						courseCode += ch;
+					}else {
+						coursePrefix += ch;
+					}
+				}
+				
+				Set<Combo> combos = new HashSet<>();
+				if(lecture.hasQuiz()) {
+					for(Quiz quiz : lecture.getQuizs(constraints)) {
+						combos.add(new Combo(coursePrefix, Integer.valueOf(courseCode), title, lecture, quiz));
+					}
+				}else {
+					combos.add(new Combo(coursePrefix, Integer.valueOf(courseCode), title, lecture, null));
+				}
+			}
+			
+		}
+		
+		if(sc.getCombos() != null) {
+			for(Combo combo : sc.getCombos()) {
+				courses.put(combo.getComboID(), new HashSet<>());
+				courses.get(combo.getComboID()).add(combo);
+			}
+		}
+		
+		ArrayList<Set<Combo>> arrli=new ArrayList<>();
+		for(Map.Entry<String, Set<Combo>> entry : courses.entrySet()) {
+			arrli.add(entry.getValue());
+		}
+		
+		Set<Set<Combo>> possibleSchedule = new HashSet<>();
+		
+		generateSchedules(arrli, 0, new HashSet<>(), possibleSchedule);
+		return possibleSchedule; // only for place holder (delete this comment after implementation)
+	}
+	
+	
+	private static void generateSchedules(ArrayList<Set<Combo>> courses, int courseIndex, Set<Combo> sofar, Set<Set<Combo>> possibleSchedule) {
+		if(courseIndex == courses.size()) {
+			possibleSchedule.add(new HashSet<>(sofar));
+		} else {
+			System.out.println(courses.get(courseIndex).size());
 
-		// This method can be divided into three parts
-		// 1. generate for Courses Set in sc (use validCombinations(constraints) to get
-		//   Combos, then check for conflicts, compatible Combos will be stored in a set)
-		// 2. for each set of Combos generated from step 1, check if Combos constructed
-		//   by (lectures + getQuizs(constraints) + String value from map splitted)
-		//   conflicts, compatible Combos will be added to the old Set
-		// 3. for each set of Combos generated from step 2, check if Combos from the
-		//   Combo Set in sc conflicts,
-		//   compatible Combos will be added to the old Set
-		// Finally, return the Set of Set of Combos from step 3
-		// (PS. I'll give an example: if the Set of Combos generated after step 1 is
-		//   Set1 Set2 and Set3, and Set1 of Combos conflicts with every Combo in step 2,
-		//   Set1 will not be added to the result in step 2, because when we are
-		//   generating for SlectedCourses we want every course in the Course Set, the
-		//   Lecture Map and the Combo Set to be present in the final schedule, one of
-		//   them missing means this Set of Combo can not be a possible schedule)
-		return null; // only for place holder (delete this comment after implementation)
+			for(Combo combo : courses.get(courseIndex)) {
+				if(canAdd(sofar, combo)) {
+					sofar.add(combo);
+					generateSchedules(courses, courseIndex + 1, sofar, possibleSchedule);
+					sofar.remove(combo);
+				}
+
+			}
+		}
+	}
+	
+	/**
+	 * @return true if combo have concurrent meeting times with any combo in set combos
+	 */
+	private static boolean canAdd(Set<Combo> combos, Combo combo) {
+		for(Combo e : combos) {
+			if(e.conflict(combo)) return false;
+		}
+		
+		return true;
 	}
 }
