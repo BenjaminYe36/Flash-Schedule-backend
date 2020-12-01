f121e2aa6e54bca0a42361270404ce5936a32774
nawZeroX239 <naw122333444455555@gmail.com>
2020-11-30 21:34:14 -0800

    fixed

--- Flash Schedule/src/ScheduleBuilder.java
+++ Flash Schedule/src/ScheduleBuilder.java
@@ -28,95 +28,59 @@ public class ScheduleBuilder {
 	 */
 	public static Set<Set<Combo>> generateSchedules(SelectedCourses sc, Constraints constraints) {
 		Set<Set<Combo>> possibleSchedule = new HashSet<>();
-		// use map to separate 
+		// use map to separate different courses
+		// each course is mapped to a set all valid combinations of lecture + quiz(if it has quiz)
 		Map<String, Set<Combo>> courses = new HashMap<>(); // courses that must be in schedule
 		
-		if(sc.getCourses() != null) { // #1 From our implementation of sc, if sc isn't null the inner collection could never be null (see the constructor)
-			for(Course course : sc.getCourses()) {
-				// #2 if there is no possible quiz sections with the given constraints, there is no possible schedule
-				Set<Combo> validCombos = course.validCombinations(constraints);
-				if (validCombos.isEmpty()) {
-					return possibleSchedule;
-				}
-				courses.put(course.getCourseID(), validCombos);
+		for(Course course : sc.getCourses()) {
+			Set<Combo> validCombos = course.validCombinations(constraints);
+			if (validCombos.isEmpty()) {
+				return possibleSchedule;
 			}
+			courses.put(course.getCourseID(), validCombos);
 		}
 		
-		if(sc.getLectures() != null) { // #1 same reason as line 34
-			for(Map.Entry<Lecture, String> entry : sc.getLectures().entrySet()) {
-				// extracts course code, prefix and title
-				
-				// Old code
-//				String coursePrefix="", courseCode="", value = entry.getValue();
-//				String title = value.substring(value.indexOf(':') + 1);
-//				Lecture lecture = entry.getKey();
-//				for(int i=0; i<value.length(); ++i) {
-//					char ch = value.charAt(i);
-//					if(ch == ':')  break;
-//					if(ch == ' ') continue;
-//					if(ch >= '0' && ch <= '9') {
-//						courseCode += ch;
-//					}else {
-//						coursePrefix += ch;
-//					}
-//				}
-				
-				// #3 use regex to simplify the extraction
-				// New code
-				Matcher matcher = COURSE_REGEX.matcher(entry.getValue());
-				String coursePrefix, courseCode, title;
-				if (matcher.find()) {
-					coursePrefix = matcher.group(1);
-					courseCode = matcher.group(2);
-					title = matcher.group(3);
-				} else {
-					throw new IllegalArgumentException("The course info String in Lecture map is not valid!");
-				}
-				
-				Lecture lecture = entry.getKey();
-				Set<Combo> combos = new HashSet<>();
-				if(lecture.hasQuiz()) {
-					// #2 if there is no possible quiz sections with the given constraints, there is no possible schedule
-					Set<Quiz> filteredQuizs = lecture.getQuizs(constraints);
-					if (filteredQuizs.isEmpty()) {
-						return possibleSchedule;
-					}
-					for(Quiz quiz : filteredQuizs) {
-						combos.add(new Combo(coursePrefix, Integer.valueOf(courseCode), title, lecture, quiz));
-					}
-				}else {
-					combos.add(new Combo(coursePrefix, Integer.valueOf(courseCode), title, lecture, null));
-				}
-				// #4 forgets to add the Combo Set constructed from each lecture to the courses map?
-				// Added New code
-				courses.put(coursePrefix + courseCode, combos);
+		for(Map.Entry<Lecture, String> entry : sc.getLectures().entrySet()) {
+			Matcher matcher = COURSE_REGEX.matcher(entry.getValue());
+			String coursePrefix, courseCode, title;
+			if (matcher.find()) {
+				coursePrefix = matcher.group(1);
+				courseCode = matcher.group(2);
+				title = matcher.group(3);
+			} else {
+				throw new IllegalArgumentException("The course info String in Lecture map is not valid!");
 			}
 			
-		}
-		
-		if(sc.getCombos() != null) { // #1 same reason as line 34
-			for(Combo combo : sc.getCombos()) {
-				// #2 if any combo doesn't satisfy the constraints, there is no possible schedule
-				if(!constraints.satisfyConstraints(combo)) {
+			Lecture lecture = entry.getKey();
+			Set<Combo> combos = new HashSet<>();
+			if(lecture.hasQuiz()) {
+				Set<Quiz> filteredQuizs = lecture.getQuizs(constraints);
+				if (filteredQuizs.isEmpty()) {
 					return possibleSchedule;
 				}
-				courses.put(combo.getComboID(), new HashSet<>());
-				courses.get(combo.getComboID()).add(combo);
+				for(Quiz quiz : filteredQuizs) {
+					combos.add(new Combo(coursePrefix, Integer.valueOf(courseCode), title, lecture, quiz));
+				}
+			}else {
+				combos.add(new Combo(coursePrefix, Integer.valueOf(courseCode), title, lecture, null));
 			}
+			courses.put(coursePrefix + courseCode, combos);
 		}
 		
-		// Old code
-//		ArrayList<Set<Combo>> arrli=new ArrayList<>();
-//		for(Map.Entry<String, Set<Combo>> entry : courses.entrySet()) {
-//			arrli.add(entry.getValue());
-//		}
 		
-		// #5 simplify arrayList copy by passing in the values collection (should be the same outcome?)
-		// New code
+		for(Combo combo : sc.getCombos()) {
+			if(!constraints.satisfyConstraints(combo)) {
+				return possibleSchedule;
+			}
+			courses.put(combo.getComboID(), new HashSet<>());
+			courses.get(combo.getComboID()).add(combo);
+		}
+	
+		
 		ArrayList<Set<Combo>> arrli = new ArrayList<>(courses.values());
 		
 		generateSchedules(arrli, 0, new HashSet<>(), possibleSchedule);
-		return possibleSchedule; // only for place holder (delete this comment after implementation)
+		return possibleSchedule;
 	}
 	
 	
