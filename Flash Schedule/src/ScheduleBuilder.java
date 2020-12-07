ca17ed3a63358938d8907565f368b7e439fddfe1
ygy36 <ygy36@HPS>
2020-12-06 22:53:45 -0800

    Added schedules to json logic to send to the front end

--- Flash Schedule/src/ScheduleBuilder.java
+++ Flash Schedule/src/ScheduleBuilder.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.regex.*;
+import org.json.simple.*;
 
 /**
  * An Utility class that provides methods with algorithms to generate possible
@@ -12,12 +13,13 @@ import java.util.regex.*;
  */
 public class ScheduleBuilder {
 	/**
-	 * The Regex pattern for matching Course Prefix + Course Code + Course title for convenience
+	 * The Regex pattern for matching Course Prefix + Course Code + Course title for
+	 * convenience
 	 * <p>
 	 * Course Prefix is group 1, Course Code is Group 2, Course title is Group 3
 	 */
 	public static final Pattern COURSE_REGEX = Pattern.compile("^([A-Z&]+ *[A-Z&]+) *([0-9]+) *: *(.*)");
-	
+
 	/**
 	 * @param sc          See SelectedCourses class
 	 * @param constraints the object that stores all the filters
@@ -27,20 +29,21 @@ public class ScheduleBuilder {
 	 *         When there is no possible schedules, it will return an empty Set
 	 */
 	public static Set<Set<Combo>> generateSchedules(SelectedCourses sc, Constraints constraints) {
-		Set<Set<Combo>> possibleSchedule = new HashSet<>();
+		Set<Set<Combo>> possibleSchedules = new HashSet<>();
 		// use map to separate different courses
-		// each course is mapped to a set all valid combinations of lecture + quiz(if it has quiz)
+		// each course is mapped to a set all valid combinations of lecture + quiz(if it
+		// has quiz)
 		Map<String, Set<Combo>> courses = new HashMap<>(); // courses that must be in schedule
-		
-		for(Course course : sc.getCourses()) {
+
+		for (Course course : sc.getCourses()) {
 			Set<Combo> validCombos = course.validCombinations(constraints);
 			if (validCombos.isEmpty()) {
-				return possibleSchedule;
+				return possibleSchedules;
 			}
 			courses.put(course.getCourseID(), validCombos);
 		}
-		
-		for(Map.Entry<Lecture, String> entry : sc.getLectures().entrySet()) {
+
+		for (Map.Entry<Lecture, String> entry : sc.getLectures().entrySet()) {
 			Matcher matcher = COURSE_REGEX.matcher(entry.getValue());
 			String coursePrefix, courseCode, title;
 			if (matcher.find()) {
@@ -50,65 +53,108 @@ public class ScheduleBuilder {
 			} else {
 				throw new IllegalArgumentException("The course info String in Lecture map is not valid!");
 			}
-			
+
 			Lecture lecture = entry.getKey();
 			Set<Combo> combos = new HashSet<>();
-			if(lecture.hasQuiz()) {
+			if (lecture.hasQuiz()) {
 				Set<Quiz> filteredQuizs = lecture.getQuizs(constraints);
 				if (filteredQuizs.isEmpty()) {
-					return possibleSchedule;
+					return possibleSchedules;
 				}
-				for(Quiz quiz : filteredQuizs) {
+				for (Quiz quiz : filteredQuizs) {
 					combos.add(new Combo(coursePrefix, Integer.valueOf(courseCode), title, lecture, quiz));
 				}
-			}else {
+			} else {
 				combos.add(new Combo(coursePrefix, Integer.valueOf(courseCode), title, lecture, null));
 			}
 			courses.put(coursePrefix + courseCode, combos);
 		}
-		
-		
-		for(Combo combo : sc.getCombos()) {
-			if(!constraints.satisfyConstraints(combo)) {
-				return possibleSchedule;
+
+		for (Combo combo : sc.getCombos()) {
+			if (!constraints.satisfyConstraints(combo)) {
+				return possibleSchedules;
 			}
 			courses.put(combo.getComboID(), new HashSet<>());
 			courses.get(combo.getComboID()).add(combo);
 		}
-	
-		
+
 		ArrayList<Set<Combo>> arrli = new ArrayList<>(courses.values());
-		
-		generateSchedules(arrli, 0, new HashSet<>(), possibleSchedule);
-		return possibleSchedule;
+
+		generateSchedules(arrli, 0, new HashSet<>(), possibleSchedules);
+		return possibleSchedules;
 	}
-	
-	
-	private static void generateSchedules(ArrayList<Set<Combo>> courses, int courseIndex, Set<Combo> sofar, Set<Set<Combo>> possibleSchedule) {
-		if(courseIndex == courses.size()) {
-			possibleSchedule.add(new HashSet<>(sofar));
+
+	private static void generateSchedules(ArrayList<Set<Combo>> courses, int courseIndex, Set<Combo> sofar,
+			Set<Set<Combo>> possibleSchedules) {
+		if (courseIndex == courses.size()) {
+			possibleSchedules.add(new HashSet<>(sofar));
 		} else {
 			// System.out.println(courses.get(courseIndex).size());
 
-			for(Combo combo : courses.get(courseIndex)) {
-				if(canAdd(sofar, combo)) {
+			for (Combo combo : courses.get(courseIndex)) {
+				if (canAdd(sofar, combo)) {
 					sofar.add(combo);
-					generateSchedules(courses, courseIndex + 1, sofar, possibleSchedule);
+					generateSchedules(courses, courseIndex + 1, sofar, possibleSchedules);
 					sofar.remove(combo);
 				}
 
 			}
 		}
 	}
-	
+
 	/**
-	 * @return true if combo have concurrent meeting times with any combo in set combos
+	 * @param possibleSchedules see generateSchedules
+	 * @return a json string containing all the possible format which is convenient
+	 *         for the front end to process
+	 */
+	@SuppressWarnings("unchecked")
+	public static String toJSONString(Set<Set<Combo>> possibleSchedules) {
+		JSONObject outterObj = new JSONObject();
+		JSONArray schedules = new JSONArray();
+		int i = 1;
+		for (Set<Combo> oneSchedule : possibleSchedules) {
+			JSONObject innerObj = new JSONObject();
+			JSONArray classes = new JSONArray();
+			JSONArray time = new JSONArray();
+			innerObj.put("id", i);
+			for (Combo combo : oneSchedule) {
+				JSONObject timeObjLecture = new JSONObject();
+				classes.add(combo.getLectureString());
+				MeetingTimes mtLecture = combo.getLecture().getMeetingTimes();
+				timeObjLecture.put("days", mtLecture.getDays());
+				timeObjLecture.put("lectureFrom", mtLecture.getStartTime());
+				timeObjLecture.put("lectureTo", mtLecture.getEndTime());
+				time.add(timeObjLecture);
+				if (combo.getQuiz() != null) {
+					JSONObject timeObjQuiz = new JSONObject();
+					classes.add(combo.getQuizString());
+					MeetingTimes mtQuiz = combo.getQuiz().getMeetingTimes();
+					timeObjQuiz.put("days", mtQuiz.getDays());
+					timeObjQuiz.put("lectureFrom", mtQuiz.getStartTime());
+					timeObjQuiz.put("lectureTo", mtQuiz.getEndTime());
+					time.add(timeObjQuiz);
+				}
+			}
+			innerObj.put("classes", classes);
+			innerObj.put("time", time);
+			innerObj.put("description", "Schedule " + i);
+			innerObj.put("show", false);
+			schedules.add(innerObj);
+			i++;
+		}
+		outterObj.put("schedules", schedules);
+		return outterObj.toJSONString();
+	}
+
+	/**
+	 * @return true if combo have concurrent meeting times with any combo in set
+	 *         combos
 	 */
 	private static boolean canAdd(Set<Combo> combos, Combo combo) {
-		for(Combo e : combos) {
-			if(e.conflict(combo)) return false;
+		for (Combo e : combos) {
+			if (e.conflict(combo))
+				return false;
 		}
-		
 		return true;
 	}
 }
